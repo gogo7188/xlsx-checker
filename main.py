6268d041a2bf1f356b91531dc98656756c7ad27b
gogo7188 <71008822+gogo7188@users.noreply.github.com>
2021-08-03 00:42:49 +0900

    Update  main.py
    Fixed incorrect active cell when there are multiple selections

--- main.py
+++ main.py
@@ -2,25 +2,60 @@ import openpyxl
 import glob
 import os
 
-fileDir  = r"D:/tmp/test/"
+fileDir  = r"D:/tmp/verifi/"
 fileExet = ".xlsx"
+TARGET_ZOOM_SCALE = 85
+
+def checkProperties(workbook):
+    print('***PROPERTIES CHECK***')
+    if workbook.properties.creator is None:
+        print('OK\n')
+    else:
+        print('NG')
+        print('creator is ' + workbook.properties.creator + '\n')
+
+def checkZoomScale(sheet):
+    print('+- ZOOM SCALE CHECK -+')
+    if sheet.sheet_view.zoomScale == TARGET_ZOOM_SCALE:
+        """
+        print('Zoomscale:%3d' % sheet.sheet_view.zoomScale)
+        print('Audit passed\n')
+        """
+        return True
+    elif sheet.sheet_view.zoomScale is None:
+        print('Zoomscale:100')
+    else:
+        print('Zoomscale:%3d' % sheet.sheet_view.zoomScale)
+    """
+    print('Audit NOT passed\n')
+    """
+    return False
+
+def checkActiveCell(sheet):
+    print('+- ACTIVE CELL CHECK -+')
+    for idx in range(len(sheet.sheet_view.selection)):
+        if sheet.sheet_view.selection[idx].activeCell == 'A1':
+            print('ActiveCell[%s] Selection[%s]' % (sheet.sheet_view.selection[idx].activeCell, idx))
+            """
+            print('Audit passed\n')
+            """
+            return True
+        else:
+            print('ActiveCell[%s] Selection[%s]' % (sheet.sheet_view.selection[idx].activeCell, idx))
+
+    """
+    print('Audit NOT passed')
+    """
+    return False
 
 for name in glob.glob(fileDir + '**/*' + fileExet , recursive=True):
-    print(name)
+    print('+------ File[%s] ------+' % name)
     wb = openpyxl.load_workbook(name,read_only=False)
 
-    # check properties data
-    if wb.properties.creator is None:
-        print('PROPERTIES CHECK OK')
-    else:
-        print('PROPERTIES CHECK NG')
-        print('creator:' + wb.properties.creator)
+    checkProperties(wb)
 
-    # check Zoomscale and active cell
     for sheet in wb:
-        if sheet.sheet_view.zoomScale is None:
-            print(sheet.title + '---' + 'Zoomscale:100' + '   Active cell:' + sheet.sheet_view.selection[0].activeCell)
-        else:
-            print(sheet.title + '---' + 'Zoomscale:%3d' % sheet.sheet_view.zoomScale + '   Active cell:' + sheet.sheet_view.selection[0].activeCell)
-    
-    print('')
+        print('+--- Sheet Name[%s] ---------+' % sheet.title)
+        print('OK') if checkZoomScale(sheet) == True else print('NG')
+        print('OK') if checkActiveCell(sheet) == True else print('NG')
+        print('')
